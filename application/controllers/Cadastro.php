0a5d5c011906afc5d89ea11d8365a58c2709d7b0
techtuxbr <jvlpark3@outlook.com>
2017-06-01 10:30:37 -0300

    Add files via upload

---
+++ application/controllers/Cadastro.php
@@ -0,0 +1,35 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Cadastro extends CI_Controller{
+
+  public function index(){
+      $this->load->helper('form');
+      
+      $this->load->view('partes/header');
+      $this->load->view('cadastro/index');
+      $this->load->view('partes/footer');
+  }
+   
+  public function novo(){
+      $this->load->library('form_validation');
+      $this->form_validation->set_rules('email','E-mail','required|min_length[4]|max_length[100]|is_unique[usuarios.email]');
+      $this->form_validation->set_rules('nome','Nome','required|min_length[4]|max_length[80]');
+      $this->form_validation->set_rules('senha','Senha','required|min_length[6]|max_length[100]');
+
+      if($this->form_validation->run() == FALSE){
+          
+          $erros = array('msgs' => validation_errors());
+          $this->load->helper('form');
+          $this->load->view('partes/header');
+          $this->load->view('cadastro/index', $erros);
+          $this->load->view('partes/footer');
+
+      }else{
+
+
+      }
+  
+
+  }
+
+}
---
+++ application/views/partes/header.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+   <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap.css'); ?>">
+     <title>Area 51</title>
+  </head>
+  <body>
+     <header>
+       <h1>Area de testes</h1>
+       <hr>
+     </header>
+     
